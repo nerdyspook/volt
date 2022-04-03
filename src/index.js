e31612c2d65a0dab6c05fee057978490509a6f5a
Susanto <nerdyspook007@gmail.com>
2022-04-03 14:30:48 +0530

    feat: added mockbee

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { FilterProvider } from "./contexts/FilterContext";
+import { makeServer } from "./server";
+
+// Call make Server
+makeServer();
 
 ReactDOM.render(
     <React.StrictMode>
